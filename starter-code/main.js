134f43094a2a24f3c57fec1a17d91279d389f20a
erikwithuhk <efjonsson@gmail.com>
2016-08-11 01:28:06 -0400

    create board using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
+// if (cardOne === cardTwo) {
+// 	alert('You found a match!');
+// } else {
+// 	alert('Sorry, try again.');
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+
+var createBoard = function() {
+	for (i = 0; i < 4; i++) {
+	  var newCard = document.createElement('div');
+	  newCard.setAttribute('class', 'card');
+	  gameBoard.appendChild(newCard);
+	}
+};
+
+createBoard();
