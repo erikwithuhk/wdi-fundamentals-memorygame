377d2556aa993f5f82def35d5b63178eea32f2ca
erikwithuhk <efjonsson@gmail.com>
2016-08-11 09:46:01 -0400

    finish game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,23 +1,49 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-// if (cardOne === cardTwo) {
-// 	alert('You found a match!');
-// } else {
-// 	alert('Sorry, try again.');
-// }
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
 
+var flipCard = function() {
+  if (this.getAttribute('data-card') === 'queen') {
+    this.setAttribute('style', 'background-image:url(card-queen.gif); background-size: cover; background-repeat: no-repeat;');
+  } else if (this.getAttribute('data-card') === 'king') {
+    this.setAttribute('style', 'background-image:url(card-king.gif); background-size: cover; background-repeat: no-repeat;');
+  }
+};
+
+var isTwoCards = function() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+
+};
+
+var isMatch = function(cardsInPlay) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  } else {
+    alert('Sorry, try again.');
+  }
+
+  var allCards = document.querySelectorAll('.card')
+  for (i = 0; i < allCards.length; i++) {
+    allCards[i].removeAttribute('style');
+  }
+};
+
 
 var createBoard = function() {
-	for (i = 0; i < 4; i++) {
-	  var newCard = document.createElement('div');
-	  newCard.setAttribute('class', 'card');
-	  gameBoard.appendChild(newCard);
-	}
+  for (i = 0; i < cards.length; i++) {
+    var newCard = document.createElement('div');
+    newCard.setAttribute('class', 'card');
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', flipCard);
+    newCard.addEventListener('click', isTwoCards);
+    gameBoard.appendChild(newCard);
+  }
 };
 
 createBoard();
